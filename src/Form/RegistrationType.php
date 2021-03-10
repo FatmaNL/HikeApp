3d4216b693214b74e2350c06983cb671badf89f0
achreffnoun <achreffnoun@gmail.com>
2021-03-10 21:35:06 +0100

    authentification + changement user entity

---
+++ src/Form/RegistrationType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+            ->add('password', PasswordType::class)
+            ->add('nom')
+            ->add('prenom')
+            ->add('cin')
+            ->add('age')
+            ->add('sexe')
+            ->add('adresse')
+            ->add('tel')
+            
+            
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
