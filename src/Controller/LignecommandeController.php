a37f5f0d9fa8e1a725174ec96d5eb821a2d81f98
heniGR <heni.guirat2@gmail.com>
2021-04-02 12:07:22 +0200

    panier

--- src/Controller/LignecommandeController.php
+++ src/Controller/LignecommandeController.php
@@ -1,30 +1,74 @@
 <?php
 
 namespace App\Controller;
+use App\Repository\ProduitRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
 
 class LignecommandeController extends AbstractController
 {
-    /**
-     * @Route("/Panier", name="Cart")
+   /**
+     * @Route("/panier", name="cart_index")
      */
-    public function index(): Response
+    public function index(SessionInterface $session, ProduitRepository $ProduitRepository)
     {
+        $panier = $session->get('panier' ,[]);
+        $panierWithData = [];
+        foreach ($panier as $numproduit => $quantite){
+            $panierWithData[] = [
+            'produit' => $ProduitRepository->find($numproduit),
+            'quantite'=> $quantite
+            ];
+        }
+        $total = 0;
+        foreach ($panierWithData as $item) {
+            
+          
+
+            $totalItem= $item['produit']->getPrix() * $item['quantite'];
+            $total += $totalItem;
+        }
         return $this->render('lignecommande/index.html.twig', [
-            'controller_name' => 'LignecommandeController',
+            'items' => $panierWithData,
+            'total' =>$total
         ]);
+        
     }
-    /**
-    * @Route("/panier/add/{numproduit}", name="cart_add")
-    */
-    public function add($numproduit, Request $request)    {
-        $session = $request->getSession();
+   /**
+     * @Route("/panier/add/{numproduit}", name="cart_add")
+     */
+    public function add($numproduit , SessionInterface $session)    {
+
         $panier=$session->get('panier',[]);
-        $panier[$numproduit]=1;
+        if(!empty($panier[$numproduit])){
+            $panier[$numproduit]++;
+        }else {
+            $panier[$numproduit]=1;
+        }
+        
+
         $session->set('panier', $panier);
-        dd($session->get('panier'));
+
+       return $this->redirectToRoute('cart_index');
+
+       
     }
+    
+        /**
+         * @Route("/panier/remove/{id}", name="cart_remove")
+         */
+        public function remove($id, SessionInterface $session){
+            $panier = $session->get('panier', []);
+            if(!empty($panier[$id])){
+                unset($panier[$id]);
+            }
+            $session->set('panier', $panier);
+            return $this->redirectToRoute('cart_index');
+
+        }
+   
 }
--- src/Form/CommandeType.php
+++ src/Form/CommandeType.php
@@ -6,15 +6,17 @@ use App\Entity\Commande;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 
 class CommandeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('refcommande')
-            ->add('datecommande')
-            ->add('etat')
+            ->add('refcommande',TextType::class, array('label'=>'Lieu'))
+            ->add('datecommande',DateType::class, array('label'=>'Date '))
+            ->add('etat',TextType::class, array('label'=>'Description '))
         ;
     }
 
